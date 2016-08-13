397a87b3b369137c649451f3c0ad3d47203ef3d9
Reg Reyes <reyesreg@yahoo.com>
2016-08-13 22:08:16 +0800

    Fixed Todo

---
+++ src/index.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import CreateTodo from './components/create-todo.js';
+import TodosList from './components/todos-list.js';
+
+import { Route, Router, ReactRouter, RouteHandler, DefaultRoute, State, IndexRoute, IndexLink, Link, Redirect } from 'react-router';
+
+function loadJSON()
+{
+    var xhr = new XMLHttpRequest();
+    var data;
+    var arr = [];
+
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                data = JSON.parse(xhr.responseText);
+            }
+        }
+    };
+    xhr.open("GET", 'https://spreadsheets.google.com/feeds/list/1xTPGQW3yftKYlNUKT7xjZl780hzQYTyBJq4-XSmohtY/od6/public/basic?alt=json', false);
+    xhr.send();
+
+    for (var i = 0; i < data.feed.entry.length; i++) {
+
+        var x = data.feed.entry[i]['content']['$t'].split(':');
+        var isCompletedVar;
+
+        if(x[1].toLowerCase() === " true") {
+            isCompletedVar = true;
+        } else {isCompletedVar = false;}
+
+        arr.push({
+            task: data.feed.entry[i]['title']['$t'],
+            isCompleted: isCompletedVar
+        });
+    }
+    return arr;
+}
+
+var todos = loadJSON();
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props); //connects to parent??
+
+        this.state = {
+            todos
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="jumbotron">
+                    <h1>React To Do</h1>
+                    <p>This is a simple to-do list app that uses Google Sheets as its backend database.
+                    <br/>Made by <strong>@reyesreg</strong></p>
+                </div>
+                <CreateTodo 
+                    todos={this.state.todos}
+                    createTask={this.createTask.bind(this)}
+                />
+                <TodosList
+                    todos={this.state.todos}
+                    toggleTask={this.toggleTask.bind(this)}
+                    saveTask={this.saveTask.bind(this)}
+                    deleteTask={this.deleteTask.bind(this)}
+                />
+            </div>
+        );
+    }
+
+    toggleTask(task) {
+        const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+        foundTodo.isCompleted = !foundTodo.isCompleted;
+        this.setState({ todos: this.state.todos });
+    }
+
+    createTask(task) {
+        this.state.todos.push({
+            task,
+            isCompleted: false
+        });
+        this.setState({ todos: this.state.todos });
+    }
+
+    saveTask(oldTask, newTask) {
+        const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+        foundTodo.task = newTask;
+        this.setState({ todos: this.state.todos });
+    }
+
+    deleteTask(taskToDelete) {
+        _.remove(this.state.todos, todo => todo.task === taskToDelete);
+        this.setState({ todos: this.state.todos });
+    }
+};
+
+ReactDOM.render(<App/>,document.querySelector("#app")
+); 
